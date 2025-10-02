41653fd51adad2f0582d9273909f3f7c15d6b796
Yi Lee <ylee1228@umd.edu>
2025-10-02 01:07:39 -0400

    Use tree for VToC too

--- BeliefQ/src/main/Decide.scala
+++ BeliefQ/src/main/Decide.scala
@@ -5,36 +5,6 @@ package beliefq
 import spinal.core._
 import spinal.lib._
 
-class SumOfMessages(params: BeliefQParams, n: Int) extends Component {
-  import params._
-  val messages = in port Vec.fill(n)(message_t())
-  val result = out port message_t()
-  var delays = 1
-  val sz = (n + 1) / 2
-  var terms = Reg(Vec.fill(sz)(message_t()))
-  for(j <- 0 until sz) {
-    if(2 * j + 1 != messages.size) {
-      terms(j) := (messages(2 * j) + messages(2 * j + 1)).truncated
-    } else {
-      terms(j) := messages(2 * j)
-    }
-  }
-  while(terms.size != 1) {
-    val new_sz = (terms.size + 1) / 2
-    var next_terms = Reg(Vec.fill(new_sz)(message_t()))
-    for(j <- 0 until new_sz) {
-      if(2 * j + 1 != terms.size) {
-        next_terms(j) := (terms(2 * j) + terms(2 * j + 1)).truncated
-      } else {
-        next_terms(j) := terms(2 * j)
-      }
-    }
-    terms = next_terms
-    delays += 1
-  }
-  result := terms(0)
-}
-
 class Decide(params: BeliefQParams, num_messages: Int) extends Component {
   import params._
   /* -- IO -- */
--- BeliefQ/src/main/VToC.scala
+++ BeliefQ/src/main/VToC.scala
@@ -14,57 +14,16 @@ class VToC(params: BeliefQParams, deg: Int) extends Component {
   /* -- IO -- */
   val inputs = in port Flow(VToCInputs(params, deg))
   val output = out port Flow(Vec.fill(deg)(message_t()))
-  val delays = 2 * deg + 1
   /* logic */
-  // Forward + backward sweep trick
-  object LocalState extends SpinalEnum {
-    val idle, forward, backward, done = newElement()
+  val sumMessages = new SumOfMessages(params, deg + 1)
+  for(i <- 0 until deg) {
+    sumMessages.messages(i) := inputs.messages(i)
   }
-  val state = RegInit(LocalState.idle)
-  val ctr = Reg(UInt(log2Up(deg) bits)) init(0)
-  val accumulator = Reg(message_t)
-  // Potentially unnecessary
-  val messages = Vec.fill(deg)(Reg(message_t))
-  val result = Vec.fill(deg)(Reg(message_t))
-  output.valid := (state === LocalState.done)
-  output.payload := result
-  switch(state) {
-    is(LocalState.idle) {
-      when(inputs.valid) {
-        state := LocalState.forward
-        accumulator := inputs.prior
-        ctr := 0
-        for(i <- 0 until deg) {
-          messages(i) := inputs.messages(i)
-        }
-      }
-    }
-    is(LocalState.forward) {
-      result(ctr) := accumulator
-      when(ctr =/= deg - 1) {
-        if(deg > 1) {
-          ctr := ctr + 1
-        }
-        accumulator := (accumulator + messages(ctr)).truncated
-      } otherwise {
-        state := LocalState.backward
-        accumulator := BigDecimal(0)
-      }
-    }
-    is(LocalState.backward) {
-      result(ctr) := (result(ctr) + accumulator).truncated
-      when(ctr =/= 0) {
-        if(deg > 1) {
-          ctr := ctr - 1
-        }
-        accumulator := (accumulator + messages(ctr)).truncated
-      } otherwise {
-        state := LocalState.done
-      }
-    }
-    is(LocalState.done) {
-      state := LocalState.idle
-    }
+  sumMessages.messages(deg) := inputs.prior
+  for(i <- 0 until deg) {
+    output.payload(i) := (sumMessages.result - inputs.messages(i)).truncated
   }
+  val delays = sumMessages.delays
+  output.valid := Delay(inputs.valid, delays, init=False)
 }
 
